0426203587be842f0d3348a7687a5200df2682ee
fatma blagui <blaguifatma02@gmail.com>
2020-10-17 23:17:23 +0200

    Facebook Button

--- app/src/main/java/com/example/authenticationfatma/LoginActivity.java
+++ app/src/main/java/com/example/authenticationfatma/LoginActivity.java
@@ -22,6 +22,50 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.annotations.Nullable;
+
+import java.util.Arrays;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -47,7 +91,7 @@ public class LoginActivity extends AppCompatActivity {
     private GoogleSignInClient mGoogleSignInClient;
     private  String TAG = "LoginActivity";
     private int RC_SIGN_IN = 1;
-    LoginButton loginFacebook ;
+    Button loginbutton ;
     private AccessTokenTracker accessTokenTracker;
     private static String Tag="Facebook authentication";
     TextView submit ,textView2;
@@ -68,28 +112,36 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         FacebookSdk.sdkInitialize(getApplicationContext());
-        loginFacebook=findViewById(R.id.login_button);
-        loginFacebook.setReadPermissions("email","public_profile");
-        callbackManager = CallbackManager.Factory.create();
-        loginFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        loginbutton=findViewById(R.id.loginbutton);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(LoginResult loginResult) {
-                Log.d(Tag,"OnSuccess"+ loginResult);
-                handleFacebookToken(loginResult.getAccessToken());
-            }
+            public void onClick(View v) {
+                callbackManager = CallbackManager.Factory.create();
+                LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this,Arrays.asList("email","public_profile"));
+                LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Log.d(Tag,"OnSuccess"+ loginResult);
+                        handleFacebookToken(loginResult.getAccessToken());
+                    }
 
-            @Override
-            public void onCancel() {
-                Log.d(Tag,"OnCancel");
+                    @Override
+                    public void onCancel() {
+                        Log.d(Tag,"OnCancel");
 
-            }
+                    }
 
-            @Override
-            public void onError(FacebookException error) {
-                Log.d(Tag,"OnError" + error);
+                    @Override
+                    public void onError(FacebookException error) {
+                        Log.d(Tag,"OnError" + error);
 
+                    }
+                });
             }
         });
+
+
+
         mAuthStateListener=new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
